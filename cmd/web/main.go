d3bcfd9590621d823c3ccced47be377845d7d77a
win <w@win.com>
2021-02-25 10:34:53 +0800

    MOVE main.go TO ROOT PATH

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,31 +1,34 @@
 package main
 
 import (
-	"flag"
 	"gfile/internal/handler"
-	"log"
-	"net/http"
 )
 
+// MOVE main.go TO ROOT PATH !
+
 func main() {
-	var PORT = flag.String("p", ":8000", "TCP address port")
-	var DIR = flag.String("d", ".", "User's directory")
-	flag.Parse()
+	// var PORT = flag.String("p", ":8000", "TCP address port")
+	// var DIR = flag.String("d", ".", "Monitor directory")
+	// var SERVICE = flag.String("s", "", "Control the system service.")
+	// flag.Parse()
 
-	// configuration
-	config, db := handler.Config(*DIR)
-	defer db.Close()
+	// // configuration
+	// config, db := handler.Config(*DIR)
+	// defer db.Close()
 
-	go config.File.Watchdog(*DIR)
+	// go config.File.Watchdog(*DIR)
 
-	server := &http.Server{
-		Addr:    *PORT,
-		Handler: config.Route(),
-	}
+	// server := &http.Server{
+	// 	Addr:    *PORT,
+	// 	Handler: config.Route(),
+	// }
 
-	log.Println("Starting...", server.Addr)
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Println(err)
-	}
+	// log.Println("Starting...", server.Addr)
+	// err := server.ListenAndServe()
+	// if err != nil {
+	// 	log.Println(err)
+	// }
+
+	// handler.Service(*PORT, *DIR, *SERVICE)
+	handler.Service()
 }
